f14adaef6fcdd36b2f74a485099c21707185a2a0
Olga Levina <olgalevina@Olgas-MacBook-Pro.local>
2018-10-09 08:42:17 -0700

    responsive

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ var config = {
     projectId: "train-scheduler-58144",
     storageBucket: "train-scheduler-58144.appspot.com",
     messagingSenderId: "75802218402"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
 $("#add-train").on("click", function (event) {
 
@@ -19,12 +19,12 @@ $("#add-train").on("click", function (event) {
     var trainTime = $("#train-time").val().trim();
     var trainFrequency = $("#train-frequency").val().trim();
 
-//    trainName.html("");
+    //    trainName.html("");
 
     database.ref().push({
         name: trainName,
         destination: trainDestination,
-        time: trainTime ,
+        time: trainTime,
         frequency: trainFrequency
     });
 
@@ -35,50 +35,33 @@ $("#add-train").on("click", function (event) {
 database.ref().on("child_added", function (snapshot) {
 
     // Log everything that's coming out of snapshot
-    console.log(snapshot.val().name);
-    console.log(snapshot.val().destination);
-    console.log(snapshot.val().time);
-    console.log(snapshot.val().frequency);
 
     var now = moment().format("HH:mm");
-    console.log("this is my now var:" + now);
     var ftime = snapshot.val().time;
-    console.log("this is my ftime: " + ftime)
     var splitTimes = ftime.split(":");
     var hr = splitTimes[0];
-   // hr = moment.hour(parseInt(hr));
-     var sett = moment().set("hour", hr);
+    var sett = moment().set("hour", hr);
     var min = splitTimes[1];
-    sett.set('minute',min);
-    console.log("this is my sett var: " + sett)
+    sett.set('minute', min);
+    var nt = next(sett, snapshot.val().frequency);
+    var first = moment(hr).minute(min);
+    first = moment(first).format("HH:mm")
+    var na = moment(nt).fromNow('mm');
+
+
+    $("#appendedItems").append("<tr><td> " + snapshot.val().name + "</td><td>" + snapshot.val().destination + "</td><td>" + snapshot.val().frequency + "</td><td>" + moment(nt).format("HH:mm") + "</td><td>" + na);
     
-    var nt = next(sett,snapshot.val().frequency);
-
-
-    var first = moment(hr).minute(min) ;
-    first =moment(first).format("HH:mm")
-    console.log( "this is my splitTime var " + hr);
-    console.log( "this is my min var " + nt);
-    //console.log(
-        var na = moment(nt).fromNow('mm');
-    // var nextTrain = function(){
-    //     moment(first.add(snapshot.val().frequency));
-    // console.log(nextTrain);
-  
-    // };
-
-    $("#appendedItems").append("<tr><td> " + snapshot.val().name + "</td><td>" + snapshot.val().destination  +"</td><td>" + snapshot.val().frequency + "</td><td>"+ moment(nt).format("HH:mm") + "</td><td>" + na + "</td><td>");
 
     // Handle the errors
 }
     , function (errorObject) {
-        console.log("Errors handled: " + errorObject.code);
+
     });
 
-    var next = function(startTime,freq){
-        while(startTime<moment()){
-        startTime.add(freq,'m')
-        }
-        
-    return startTime;
+    var next = function (startTime, freq) {
+    while (startTime < moment()) {
+        startTime.add(freq, 'm')
     }
+
+    return startTime;
+}
